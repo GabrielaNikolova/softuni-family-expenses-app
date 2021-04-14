b56cbed06f18f3aa08fc2f71c871ad6b060f1148
gnikolova <gnikolova@ymail.com>
2021-04-15 00:41:37 +0300

    updated dashboard

--- src/main/java/com/familyapp/repositories/IncomeRepo.java
+++ src/main/java/com/familyapp/repositories/IncomeRepo.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IncomeRepo extends JpaRepository<Income, Long> {
 
-    @Query(nativeQuery = true, value = "select income_amount from family_app_db.incomes where added_by_id=:id order by received_on desc limit 1")
+    @Query(nativeQuery = true, value = "SELECT SUM(income_amount) FROM family_app_db.incomes WHERE added_by_id=:id GROUP BY YEAR(received_on), MONTH(received_on) ORDER BY YEAR(received_on) DESC, MONTH(received_on) DESC LIMIT 1")
     Long findIncomeAmountByUser(@Param("id") Long id);
 
     @Query("SELECT SUM(i.savingsAmount) FROM Income AS i WHERE i.addedBy.id=:id")
